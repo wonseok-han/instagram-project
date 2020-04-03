8c564bbc2ecbb5b510e6fa5b7c7011cf34490209
HanWonSeok <oshan1112@gmail.com>
2020-04-03 17:39:27 +0900

    frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import BrowserRouter from "react-router-dom";
 import "./index.css";
-import App from "./App";
+import Root from "pages";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Root />
+  </BrowserRouter>,
   document.getElementById("root")
 );
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Root = () => {
+  return <div>최상위 컴포넌트</div>;
+};
+
+export default Root;
