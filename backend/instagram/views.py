ac21ce87942985ab6aa1f77a1d65310c8a22fa85
HanWonSeok <oshan1112@gmail.com>
2020-04-06 22:51:59 +0900

    backend

--- backend/instagram/views.py
+++ backend/instagram/views.py
@@ -8,17 +8,19 @@ from .models import Post
 from .serializers import PostSerializer
 
 # Create your views here.
+
+
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     # permission_classes = [AllowAny]  # FIXME: 인증적용
 
-    # def get_queryset(self):
-    #     timesince = timezone.now() - timedelta(days=3)
-    #     qs = super().get_queryset()
-    #     qs = qs.filter(
-    #         Q(author=self.request.user)
-    #         | Q(author__in=self.request.user.following_set.all())
-    #     )
-    #     qs = qs.filter(created_at__qte=timesince)
-    #     return qs
+    def get_queryset(self):
+        timesince = timezone.now() - timedelta(days=3)
+        qs = super().get_queryset()
+        qs = qs.filter(
+            Q(author=self.request.user)
+            | Q(author__in=self.request.user.following_set.all())
+        )
+        qs = qs.filter(created_at__gte=timesince)
+        return qs
