a3d73fa83386d5f0c599bc505e6f014e94cd47f3
HanWonSeok <oshan1112@gmail.com>
2020-04-07 18:40:23 +0900

    Updates

---
+++ precache-manifest.27d05014819997d5a37c72ce7ddb03aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b2e211cc576954e308368c4bcab32c",
+    "url": "/instagram-project/index.html"
+  },
+  {
+    "revision": "4d0187d76275928dd510",
+    "url": "/instagram-project/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "e7fd75efb3d0f6898ec0",
+    "url": "/instagram-project/static/css/main.68aaadaa.chunk.css"
+  },
+  {
+    "revision": "4d0187d76275928dd510",
+    "url": "/instagram-project/static/js/2.213df2c6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/instagram-project/static/js/2.213df2c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7fd75efb3d0f6898ec0",
+    "url": "/instagram-project/static/js/main.8fde3cea.chunk.js"
+  },
+  {
+    "revision": "d02d5f9faea34ee651ca",
+    "url": "/instagram-project/static/js/runtime-main.6af247b2.js"
+  },
+  {
+    "revision": "dcc389e54c4374fb8ccf2e8fc2ecedf9",
+    "url": "/instagram-project/static/media/instagram_logo.dcc389e5.png"
+  }
+]);
