fe68ed9256d3a7b6575ab1e680781862f409da23
HanWonSeok <oshan1112@gmail.com>
2020-04-07 18:31:24 +0900

    backend

--- backend/instagram/views.py
+++ backend/instagram/views.py
@@ -2,10 +2,14 @@ from django.db.models import Q
 from django.shortcuts import render
 from django.utils import timezone
 from datetime import timedelta
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny
-from .models import Post
-from .serializers import PostSerializer
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
 # Create your views here.
 
@@ -19,6 +23,11 @@ class PostViewSet(ModelViewSet):
     serializer_class = PostSerializer
     # permission_classes = [AllowAny]  # FIXME: 인증적용
 
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["request"] = self.request
+        return context
+
     def get_queryset(self):
         timesince = timezone.now() - timedelta(days=3)
         qs = super().get_queryset()
@@ -31,4 +40,36 @@ class PostViewSet(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+        return super().perform_create(serializer)
+
+    @action(detail=True, methods=["POST"])
+    def like(self, request, pk):
+        post = self.get_object()
+        post.like_user_set.add(self.request.user)
+        return Response(status.HTTP_201_CREATED)
+
+    @like.mapping.delete
+    def unlike(self, request, pk):
+        post = self.get_object()
+        post.like_user_set.remove(self.request.user)
+        return Response(status.HTTP_204_NO_CONTENT)
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["request"] = self.request
+        return context
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(post__pk=self.kwargs["post_pk"])
+        return qs
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs["post_pk"])
+        serializer.save(author=self.request.user, post=post)
         return super().perform_create(serializer)
